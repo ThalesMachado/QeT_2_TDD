99df099aa66cd96a5e53f35e726d68ec5c2cb778
Thales de Souza Machado <thalesm@ipiranga.ipiranga>
2021-02-24 23:34:21 -0300

    Criação de Teste

---
+++ src/main/java/com/QeT/App/Fatura/FaturaBO.java
@@ -0,0 +1,5 @@
+package main.java.com.QeT.App.Fatura;
+
+public class FaturaBO {
+
+}
---
+++ src/test/java/com/QeT/App/FaturaTest.java
@@ -0,0 +1,23 @@
+package test.java.com.QeT.App;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import main.java.com.QeT.App.Boleto.Boleto;
+import main.java.com.QeT.App.Fatura.Fatura;
+import main.java.com.QeT.App.Fatura.FaturaBO;
+
+public class FaturaTest {
+    @Test
+    public void testaPagamentoFatura() {
+        List<Boleto> boletos = new ArrayList<Boleto>();
+        boletos.add(new Boleto(100, new Date(), "12345"));
+        Fatura fatura = new Fatura(new Date(), 100.0, "João Semgraça");
+        Boolean faturaEstaPaga = FaturaBO.faturaEstaPaga(fatura, boletos);
+        Assertions.assertEquals(true, faturaEstaPaga);
+    }
+}
