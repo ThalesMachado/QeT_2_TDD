7435722c3784ab1bd40a04647ec36b0710aab00e
Thales Machado <thales.machado@sysmap.com.br>
2021-02-25 12:44:43 -0300

    Configuração de classe para atender aos testes

--- src/main/java/com/QeT/App/Boleto/BoletoBO.java
+++ src/main/java/com/QeT/App/Boleto/BoletoBO.java
@@ -1,5 +1,23 @@
 package com.QeT.App.Boleto;
 
+import java.util.List;
+
 public class BoletoBO {
+
+    private static final BoletoBO instance = new BoletoBO();
+    
+    public static BoletoBO getInstance(){
+        return instance;
+    }
+
+    public Double somatorioBoletos(List<Boleto> boletos){
+        Double resultado = 0.0;
+
+        for (Boleto boleto : boletos) {
+            resultado += boleto.getValorPago();
+        }
+
+        return resultado;
+    }
 
 }
